6c04e7081e0809c6134cfc4b41b9e13cd5de5756
Duchung14 <147479162+Duchung14@users.noreply.github.com>
2023-11-10 20:54:52 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-function numberOneTriangle(5) {
-    
-    if (n < 1 || n > 10) {
-        console.log("5");
-        return;
-    }
+const express = require('express');
+const app = express();
+const port = 3000;
 
-    // In ra hình tam giác
-    for (let i = 1; i <= n; i++) {
-        let row = '';
-        for (let j = 1; j <= i; j++) {
-            row += '1 ';
-        }
-        console.log(row);
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/calculate', (req, res) => {
+    const a = parseInt(req.body.a);
+    const b = parseInt(req.body.b);
+    const result = calculatePrimeSum(a, b);
+    res.json({ result });
+});
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
     }
+    return true;
 }
 
+function calculatePrimeSum(a, b) {
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        if (isPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
